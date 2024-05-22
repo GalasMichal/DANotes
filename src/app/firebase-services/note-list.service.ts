f0b87ea079f9e99b3019a12515ca0cff9862d178
Michal Galas <michalgalas1989@gmail.com>
2024-05-22 21:52:18 +0200

    some changes

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -14,49 +14,67 @@ import { Observable } from 'rxjs';
 })
 export class NoteListService {
   firestore: Firestore = inject(Firestore);
-  items$; //// entwickler nutzen dollar wenn die was observable ist ud wird damit gekennzeichnet
-  items;
-  unsubList;
-  
+  // items$; //// entwickler nutzen dollar wenn die was observable ist ud wird damit gekennzeichnet
+  // items;
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubTrash;
+  unsubNotes;
+
   constructor() {
-    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
-      list.forEach((element) => {
-        console.log(this.setNoteObject(element.data(), element.id));
-      });
-    }); /// onsnapshot brauch eine referenz und eine funktion
-
-   
-     
-
-    // eine von lösungen zum echzeitdata abzurufen
-    this.items$ = collectionData(this.getNotesRef());
-    this.items = this.items$.subscribe((list) => {
-      list.forEach((element) => {
-        console.log(element);
-      });
-    });
-    // das sollte man immer nutzen um beenden darauf zu hören oder die appcomp wird geschlossen
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
   }
 
-  setNoteObject(obj: any, id:string): Note { /// mit diese funktion wird abgedeckt falls was in databank nicht existiert
+  //   // eine von lösungen zum echzeitdata abzurufen
+  //   this.items$ = collectionData(this.getNotesRef());
+  //   this.items = this.items$.subscribe((list) => {
+  //     list.forEach((element) => {
+  //       console.log(element);
+  //     });
+  //   });
+  //   // das sollte man immer nutzen um beenden darauf zu hören oder die appcomp wird geschlossen
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      /// onsnapshot brauch eine referenz und eine funktion
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach((element) => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    /// mit diese funktion wird abgedeckt falls was in databank nicht existiert
     return {
       id: id,
-      type: obj.type || "note",
-      title: obj.title || "",
-      content: obj.content || "",
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
       marked: obj.marked || false,
-
-    }
+    };
   }
 
   ngOnDestroy(): void {
-    this.items.unsubscribe(); /// hier wird es damit wieder unsubscribe
-    
-    this.unsubList();
+    // this.items.unsubscribe(); /// hier wird es damit wieder unsubscribe
+
+    this.unsubTrash();
+    this.unsubNotes();
   }
 
   getNotesRef() {
-    return collection(this.firestore, 'notes'); // collection ist die samlung auf firebase
+    return collection(this.firestore, 'notes'); // collection ist die sammlung auf firebase
   }
 
   getTrashRef() {
